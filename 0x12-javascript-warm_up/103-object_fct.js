c810aceaedc11844497ff3ad55501aa0a08ef79e
Adefemi Adelugba <adefemiade3662@gmail.com>
2023-03-15 10:01:02 +0100

    Javascript is amazing

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
+let myObject = {
+	type: 'object',
+	value: 12
 };
 console.log(myObject);
 
 myObject.incr = function () {
-  this.value++;
+	this.value++;
 };
 
 myObject.incr();
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add (a, b) {
-  return a + b;
+function addNumbs (a, b) {
+	const c = a + b;
+	console.log(c);
 }
-
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+addNumbs(Number(process.argv[2]), Number(process.argv[3]));

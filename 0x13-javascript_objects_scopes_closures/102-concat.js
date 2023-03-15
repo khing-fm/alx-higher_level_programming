1732dd75c6706b42bf6f297830dd58dfe8a80f8d
Adefemi Adelugba <adefemiade3662@gmail.com>
2023-03-15 11:16:14 +0100

    Javascript is amazing

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const path = require('path');
 
-// Get the file paths from the command-line arguments
-const [source1, source2, destination] = process.argv.slice(2);
-
-// Read the contents of the source files
-const data1 = fs.readFileSync(path.resolve(__dirname, source1));
-const data2 = fs.readFileSync(path.resolve(__dirname, source2));
-
-// Concatenate the contents of the source files
-const newData = Buffer.concat([data1, data2]);
-
-// Write the concatenated data to the destination file
-fs.writeFileSync(path.resolve(__dirname, destination), newData);
-
-console.log(`Successfully concatenated ${source1} and ${source2} into ${destination}`);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
